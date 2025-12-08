84763c1c76db8639f34d7666c21d4784c4bee1e0
amar68 <amarsrivastava68@gmail.com>
2025-12-08 21:37:42 +0530

    swagger integeration

--- bookstore-api/routes/book-routes.js
+++ bookstore-api/routes/book-routes.js
@@ -1,16 +1,111 @@
 import express from 'express'
-import { getAllBooks  , updateBook , deleteBook , getSingleBookById  , addNewBook} from '../controllers/book-controller.js'
+import { getAllBooks, updateBook, deleteBook, getSingleBookById, addNewBook } from '../controllers/book-controller.js'
 
 const BookRoutes = express.Router()
 
-//all routes related to books will be here  
+/**
+ * @swagger
+ * /api/books/get:
+ *   get:
+ *     summary: Get all books
+ *     tags: [Books]
+ *     responses:
+ *       200:
+ *         description: List of all books
+ */
+BookRoutes.get('/get', getAllBooks)
 
-BookRoutes.get('/get', getAllBooks) // IF WE ARE USING QUERY PARAM , NO NEED TO DECLARE GET/:ID IN ROUTE
-                                    // WE CAN JUST USE GET WITH QUERY PARAMS IN CONTROLLER
+/**
+ * @swagger
+ * /api/books/get/{id}:
+ *   get:
+ *     summary: Get a book by ID
+ *     tags: [Books]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Book found
+ *       404:
+ *         description: Book not found
+ */
 BookRoutes.get('/get/:id', getSingleBookById)
-    
+
+/**
+ * @swagger
+ * /api/books/add:
+ *   post:
+ *     summary: Add a new book
+ *     tags: [Books]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               title:
+ *                 type: string 
+ *               author:
+ *                 type: string
+ *               year:
+ *                 type: number
+ *               
+ *     responses:
+ *       201:
+ *         description: Book created
+ */
 BookRoutes.post('/add', addNewBook)
+
+/**
+ * @swagger
+ * /api/books/update/{id}:
+ *   put:
+ *     summary: You can use this api to update the book contents using the book id . 
+ *     tags: [Books]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               title:
+ *                 type: string
+ *               author:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Book updated
+ */
 BookRoutes.put('/update/:id', updateBook)
+
+/**
+ * @swagger
+ * /api/books/delete/{id}:
+ *   delete:
+ *     summary: Delete a book
+ *     tags: [Books]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Book deleted
+ */
 BookRoutes.delete('/delete/:id', deleteBook)
 
 export default BookRoutes
--- bookstore-api/server.js
+++ bookstore-api/server.js
@@ -1,18 +1,43 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import { connectToDB } from './database/db.js'
-
+import swaggerJsdoc from 'swagger-jsdoc'
+import swaggerUi from 'swagger-ui-express'
+import BookRoutes from './routes/book-routes.js'
 
 dotenv.config()
 
 const app = express()
 const PORT = process.env.PORT || 7001
 
+// Swagger configuration
+const swaggerOptions = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Book API',
+            version: '1.0.0',
+            description: 'A simple Book API'
+        },
+        servers: [
+            { url: `http://localhost:${PORT}` }
+        ]
+    },
+    apis: ['./routes/*.js'] // Path to your route files
+}
+
+const swaggerSpec = swaggerJsdoc(swaggerOptions)
+
+// Swagger UI route
+app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
+
+
 //connection to the database .
 connectToDB()
 
 //middleware to parse json data.
 app.use(express.json())
+app.use('/api/books' , BookRoutes)
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
